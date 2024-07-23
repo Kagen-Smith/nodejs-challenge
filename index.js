9ba72a9c74b7330bb708fd8dd951a511655a7f35
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-07-22 22:33:17 -0600

    added inquirer, and started code for the questions

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
-
+import inquirer from "inquirer";
+import fs from "fs";
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    inquirer
+    .prompt ([
+        {
+            type: 'input',
+            message: "what is the title of your project?",
+            name: "project title"
+        },
+        {
+           type: 'input',
+           message: 'Please enter a description of your project'
+           name: "description" 
+        },
+        {
+            type: 'input',
+            message 'what are the instilation instructions?',
+            name: 'instilation instructions'
+        },
+        {
+            type: 'input',
+            message:" what are the contribution guidelines?",
+            name: 'contribution guidelines'
+        },
+        {
+            type: 'input', 
+            message: "What tests were done on the project?"
+        }
+    ])
+    .then 
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
