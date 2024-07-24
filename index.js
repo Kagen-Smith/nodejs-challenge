ef183382d37535b84d0ca700dee30140f0110f0c
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-07-24 08:57:53 -0600

    added functions to write a README file, and a fuction to initialize the app

--- index.js
+++ index.js
@@ -1,43 +1,69 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
+import { stderr } from "process";
 // TODO: Create an array of questions for user input
 const questions = [
     inquirer
-    .prompt ([
-        {
-            type: 'input',
-            message: "what is the title of your project?",
-            name: "project title"
-        },
-        {
-           type: 'input',
-           message: 'Please enter a description of your project'
-           name: "description" 
-        },
-        {
-            type: 'input',
-            message 'what are the instilation instructions?',
-            name: 'instilation instructions'
-        },
-        {
-            type: 'input',
-            message:" what are the contribution guidelines?",
-            name: 'contribution guidelines'
-        },
-        {
-            type: 'input', 
-            message: "What tests were done on the project?"
-        }
-    ])
-    .then 
+        .prompt([
+            {
+                type: 'input',
+                message: "what is the title of your project?",
+                name: "project title"
+            },
+            {
+                type: 'input',
+                message: 'Please enter a description of your project',
+                name: 'description'
+            },
+            {
+                type: 'input',
+                message: 'what are the instilation instructions?',
+                name: 'instilation instructions'
+            },
+            {
+                type: 'input',
+                message: " what are the contribution guidelines?",
+                name: 'contribution guidelines'
+            },
+            {
+                type: 'input',
+                message: "What tests were done on the project?",
+                name: 'tests'
+            }
+        ])
+        .then
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+fs.writeFile('README.md', questions, (err) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('README.md file created successfully!');
+});
+
 
 // TODO: Create a function to initialize app
-function init() {}
+const { exec } = require(`inquirer`)
+function init() {
+    exec(`npm init -y`, (error, stderr) => {
+        if (error) {
+            console.error(`There was an error initializing Node.js app: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.error(`There was an error initializing Node.js app: ${stderr}`);
+            return;
+        }
+        console.log(`Node.js app was initialized successfully!`);
+    });
+} 
+
+
+
+
 
 // Function call to initialize app
 init();
