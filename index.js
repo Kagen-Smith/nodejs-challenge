18ee57bd3ebb1eabf028c0f64e7509fd2323a6c6
Kagen Smith <kagensmith@Smiths-iMac.local>
2024-07-29 17:02:44 -0600

    i put the markdown info in the correct spot. I also edited the function to write to file, and the initialized app function

--- index.js
+++ index.js
@@ -1,89 +1,84 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
 import fs from "fs";
-import license  from './utils/generateMarkdown.js'
+import generateMarkdown from './utils/generateMarkdown.js'
 
 // TODO: Create an array of questions for user input
 const questions = [
-            {
-                type: 'input',
-                message: "what is the title of your project?",
-                name: "title"
-            },
-            {
-                type: 'input',
-                message: 'Please enter a description of your project',
-                name: 'description'
-            },
-            {
-                type: 'input',
-                message: 'please enter the table of contents',
-                name: 'Toc'
-            },
-            {
-                type: 'input',
-                message: 'what are the instilation instructions?',
-                name: 'instilation'
-            },
-            {
-                type: 'input',
-                message: 'what is the usage for your project?',
-                name: 'usage'
-            },
-            {
-                type: 'input',
-                message: " what are the contribution guidelines?",
-                name: 'contribution'
-            },
-            {
-                type: 'input',
-                message: "What tests were done on the project?",
-                name: 'tests'
-            },
-            {
-                type: 'input',
-                message: "what is your Github link?",
-                name: 'github'
-            },
-            {
-                type: 'input',
-                message: 'what is your email?',
-                name: 'email'
-            },
-            {
-                type: 'list',
-                message: 'please select your license',
-                name: `license`,
-                choices:['MIT license, GPL 3.0 license, Apache 2.0 license']
-            }
-        ];
+    {
+        type: 'input',
+        message: "what is the title of your project?",
+        name: "title"
+    },
+    {
+        type: 'input',
+        message: 'Please enter a description of your project',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'what are the instillation instructions?',
+        name: 'instillation'
+    },
+    {
+        type: 'input',
+        message: 'what is the usage for your project?',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: " what are the contribution guidelines?",
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: "What tests were done on the project?",
+        name: 'tests'
+    },
+    {
+        type: 'input',
+        message: "what is your Github link?",
+        name: 'github'
+    },
+    {
+        type: 'input',
+        message: 'what is your email?',
+        name: 'email'
+    },
+    {
+        type: 'list',
+        message: 'please select your license',
+        name: `license`,
+        choices: ['MIT', 'GPL 3.0', 'Apache 2.0 license', 'none']
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writefile(fileName, data, (err) => {
+    const markdownItems = generateMarkdown(data);
+    fs.writefile(fileName, markdownItems, (err) => {
         if (err) {
             console.error(err);
         } else {
             console.log('Your README has been generated!')
         }
     })
- 
- };
+
+};
 
 
 // TODO: Create a function to initialize app
 
-function init(writeToFile) {
-    inquirer.prompt(questions)
-    .then((answers) => {
-        const data = `# ${answers.title}\n\n## Description\n${answers.description}\n\n## Table of Contents\n${answers.Toc}\n\n## Instilation Instructions\n${answers.isntilation}\n\n## Usage\n${answers.usage}\n\n## Contribution Guidelines\n${answers.contribution}\n\n## Test instructions\n${answers.tests}\n\n## About\n${answers.github}\n${answers.email}\n\n## License\n${renderLicenseBadge}\n${renderLicenseLink}\n${renderLicenseSection}\n`;
-        const fileName = 'README.md';
-        writeToFile(fileName, data);
-    })
-    .catch((error) => {
-        console.error(`there was an error generating the prompt:`, error);
-    });
-} 
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            writeToFile("README.md", data);
+        })
+        .catch((error) => {
+            console.error(`there was an error generating the prompt:`, error);
+        });
+}
 
 
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,11 +2,11 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'MIT') {
-  return '[![license: MIT]](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  return '[![license: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]';
 } else if (license === 'Apache 2.0') {
-  return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]';
 } else if (license === 'GPL 3.0') {
-  return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+  return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]';
 } else {
   return '';
 }
@@ -16,11 +16,11 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'MIT') {
-    return 'https://choosealicense.com/licenses/mit/';}
+    return '(https://opensource.org/licenses/MIT)';}
   else if (license === 'Apache 2.0') {
-    return 'https://choosealicense.com/licenses/apache-2.0/';}
+    return '(https://opensource.org/license/apache-2-0)';}
   else if (license === 'GPL 3.0') {
-    return 'https://choosealicense.com/licenses/gpl-3.0/';}
+    return '(https://www.gnu.org/licenses/gpl-3.0)';}
     else {
       return '';
     }
@@ -30,11 +30,11 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license === 'MIT') {
-      return 'https://choosealicense.com/licenses/mit/'
+      return 'This project uses the MIT license'
     } else if (license === 'Apache 2.0') {
-      return 'https://choosealicense.com/licenses/apache-2.0/'
+      return 'This project uses the Apache 2.0 license'
     } else if (license === 'GPL 3.0') {
-      return 'https://choosealicense.com/licenses/gpl-3.0/'
+      return 'This project uses the GPL 3.0 license'
     } else {
       return '';
     }
@@ -43,7 +43,41 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const badge = renderLicenseBadge(data.license);
+
+  return `
+${badge}${renderLicenseLink(data.license)}
+
+  # ${data.title}
+  
+  ## Description 
+  ${data.description}
+
+  ## Table of contents
+  -[Installation](#installation)
+  -[Usage](#usage)
+  -[Contribution](#contribution)
+  -[Tests](#tests)
+  -[Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contribution}
+
+  ##tests
+  ${data.tests}
+
+  ##Questions
+  please visit my Github ${data.github}.
+  For additional questions, please contact me at ${data.email}.
 
 `;
 }
